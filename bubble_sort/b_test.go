9cee456b6200ec0943f6c772b51d818f26f7e245
kostushka <nastyatery47@yandex.ru>
2025-03-03 17:30:29 +0300

    up

--- bubble_sort/b_test.go
+++ bubble_sort/b_test.go
@@ -2,12 +2,53 @@ package main
 
 import (
 	"testing"
+	"math/rand"
 )
 
-func BenchmarkBubbleSort(b *testing.B) {
-	x := []int{3, 64, 2, 6, 45, 4, 1, 67, 54, 86, 34, 24, 13}
-	
-	for i := 0; i < b.N; i++ {
-		bubbleSort(x)
+var randomArr []int
+
+func init() {
+	rand.Seed(3)
+	randomArr = make([]int, 100000)
+	for i := 0; i < len(randomArr); i++ {
+		randomArr[i] = rand.Intn(700)
 	}
+}
+
+var negative []int
+
+func init() {
+	negative = make([]int, 100000)
+	j := 0
+	for i := len(negative) - 1; i > 0; i-- {
+		negative[j] = i
+		j++
+	}
+}
+
+var sorted []int
+
+func init() {
+	sorted = make([]int, 100000)
+	for i := 0; i < len(sorted); i++ {
+		sorted[i] = i
+	}
+}
+
+func BenchmarkBubbleSort(b *testing.B) {
+	b.Run("random", func(b *testing.B) {
+		for b.Loop() {
+			bubbleSort(randomArr)
+		}
+	})
+	b.Run("negative", func(b *testing.B) {
+		for b.Loop() {
+			bubbleSort(negative)
+		}
+	})
+	b.Run("sorted", func(b *testing.B) {
+		for b.Loop() {
+			bubbleSort(sorted)
+		}
+	})
 }
--- bubble_sort/bubble_sort.go
+++ bubble_sort/bubble_sort.go
@@ -1,34 +1,29 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
 	arr := []int{21, 93, 23, 61, 82, 13, 12, 45}
-	fmt.Println("before: ", arr)
+	// fmt.Println("before: ", arr)
 	bubbleSort(arr)
-	fmt.Println("after: ", arr)
+	// fmt.Println("after: ", arr)
 }
 
 // Вычислительная сложность алгоритма: O(n^2)
 // Пространственная сложность алгоритма: О(1)
 func bubbleSort(arr []int) {
 	var count int
-	var isChange bool
-	for i := 0; i < len(arr) - 1; i++ {
+	var swapped bool = true
+	for i := 0; i < len(arr) - 1 && swapped; i++ {
 		count++
 		// fmt.Println(count)
-		
-		isChange = false
+		swapped = false
 		for j := 0; j < len(arr) - 1; j++ {
 			if arr[j] > arr[j+1] {
-				isChange = true
+				swapped = true
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
 			// fmt.Println(arr)
-			
-		}
-		if !isChange {
-			break
 		}
 	}
 }
