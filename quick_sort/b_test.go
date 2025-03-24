f414866004c2eff0e56ac76ffcfdc635019b3562
kostushka <nastyatery47@yandex.ru>
2025-03-24 09:34:15 +0300

    add quick_sort

---
+++ quick_sort/b_test.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"testing"
+	"math/rand"
+)
+
+const big = 10_000
+const middle = 5_000
+const small = 100
+
+var random []int
+
+func init() {
+	rand.Seed(3)
+	random = make([]int, big)
+	for i := 0; i < len(random); i++ {
+		random[i] = rand.Intn(700)
+	}
+}
+
+var negative []int
+
+func init() {
+	negative = make([]int, big)
+	index := 0
+	for i := len(negative); i > 0; i-- {
+		negative[index] = i
+		index++
+	}
+}
+
+var sorted []int
+
+func init() {
+	sorted = make([]int, big)
+	for i := 0; i < len(sorted); i++ {
+		sorted[i] = i
+	}
+}
+
+func BenchmarkQuickSortR(b *testing.B) {
+	b.Run("random10_000", func(b *testing.B) {
+		rcopy := make([]int, big)
+		for b.Loop() {
+			copy(rcopy, random)
+			quickSort(rcopy)
+		} 
+	})
+	b.Run("random5_000", func(b *testing.B) {
+		rcopy := make([]int, middle)
+		for b.Loop() {
+			copy(rcopy, random)
+			quickSort(rcopy)
+		}
+	})	
+	b.Run("random100", func(b *testing.B) {
+		rcopy := make([]int, small)
+		for b.Loop() {
+			copy(rcopy, random)
+			quickSort(rcopy)
+		}
+	})
+}
+
+func BenchmarkQuickSortN(b *testing.B) {
+	b.Run("negative10_000", func(b *testing.B) {
+		rcopy := make([]int, big)
+		for b.Loop() {
+			copy(rcopy, negative)
+			quickSort(rcopy)
+		} 
+	})
+	b.Run("negative5_000", func(b *testing.B) {
+		rcopy := make([]int, middle)
+		for b.Loop() {
+			copy(rcopy, negative)
+			quickSort(rcopy)
+		}
+	})	
+	b.Run("negative100", func(b *testing.B) {
+		rcopy := make([]int, small)
+		for b.Loop() {
+			copy(rcopy, negative)
+			quickSort(rcopy)
+		}
+	})
+}
+
+func BenchmarkQuickSortS(b *testing.B) {
+	b.Run("sorted10_000", func(b *testing.B) {
+		rcopy := make([]int, big)
+		for b.Loop() {
+			copy(rcopy, sorted)
+			quickSort(rcopy)
+		} 
+	})
+	b.Run("sorted5_000", func(b *testing.B) {
+		rcopy := make([]int, middle)
+		for b.Loop() {
+			copy(rcopy, sorted)
+			quickSort(rcopy)
+		}
+	})	
+	b.Run("sorted100", func(b *testing.B) {
+		rcopy := make([]int, small)
+		for b.Loop() {
+			copy(rcopy, sorted)
+			quickSort(rcopy)
+		}
+	})
+}
